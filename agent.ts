5a3c14b6cfae6c1e5e51b04b7a0ca447bfa32498
kkjonnykk <28173360+kkjonnykk@users.noreply.github.com>
2023-04-27 06:32:08 +0100

    Session management

--- agent.ts
+++ agent.ts
@@ -1,13 +1,42 @@
 import process from 'node:process';
-import bsky from '@atproto/api';
+import bsky, { AtpSessionEvent, AtpSessionData } from '@atproto/api';
 const { BskyAgent } = bsky;
 import * as dotenv from 'dotenv';
 dotenv.config();
+import fs from 'fs';
 
-export const agent = new BskyAgent({
+const agent = new BskyAgent({
   service: 'https://bsky.social',
+  persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
+    if (evt === 'create') {
+      fs.writeFileSync('sessiondata/session.json', JSON.stringify(sess))
+    } 
+  }
 });
-await agent.login({
-  identifier: process.env.BSKY_USERNAME!,
-  password: process.env.BSKY_PASSWORD!,
-});
+
+// load session from file
+let session;
+try {
+  session = JSON.parse(fs.readFileSync('session.json', 'utf8'));
+} catch (err) {
+  console.error(err);
+}
+
+// try to resume session
+try {
+  await agent.resumeSession(session);
+} catch (err) {
+  console.error(err);
+}
+
+if (!agent.hasSession) {
+  console.log('Logging in...');
+  await agent.login({
+    identifier: process.env.BSKY_USERNAME!,
+    password: process.env.BSKY_PASSWORD!,
+  });
+} else {
+  console.log("Session resumed.")
+}
+
+export default agent
--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import bsky from '@atproto/api';
-import { agent } from './agent.js';
+import agent from './agent.js';
 
+const data = await agent.post({
+  text: "other world",
+})
 
+console.log(data)
